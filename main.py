d1fbd8d9296c9a15b508d85c255726183fb43788
Steelov <102536588+Steelov@users.noreply.github.com>
2022-09-16 00:20:27 +0600

    first

--- main.py
+++ main.py
@@ -1,30 +1,16 @@
-firstNumber = tuple([eval(val) for val in input("Please enter the first number (ex: 1/5): ").split('/')])
-secondNumber = tuple([eval(val) for val in input("Please enter the second number (ex: 2/7): ").split('/')])
+import math
 
+h = int(input('Enter the height of a rectangle:'))
+w = int(input('Enter the width of a rectangle:'))
+print("The area of a rectangle with height: {0} and width: {1} is {2}".format(h, w, h * w))
 
-def multiply():
-    print(int(firstNumber[0]) * int(secondNumber[0]), '/', int(firstNumber[1] * secondNumber[1]))
+rad = int(input('Enter the radius of a circle:'))
+print("The are of the circle with radius: {0} is {1} ".format(rad, round(math.pi * rad * rad, 4)))
 
-
-def divide():
-    print(int(firstNumber[0]) * int(secondNumber[1]), '/', int(firstNumber[1] * secondNumber[0]))
-
-
-def smallest():
-    final_list = []
-    line = input("Enter the fraction:")
-    while line != '':
-        final_list.append(tuple([eval(val) for val in input("Please enter the first number (ex: 1/5): ").split('/')]))
-        line = input("Enter the fraction:")
-
-    print(final_list)
-
-
-smallest()
-
-# while True:
-#     a = input('Enter the command:')
-#     if a == "again":
-#         firstNumber = tuple([eval(val) for val in input("Please enter the first number (ex: 1/5): ").split('/')])
-#         secondNumber = tuple([eval(val) for val in input("Please enter the second number (ex: 2/7): ").split('/')])
-#         print(firstNumber)
+height = int(input("Enter the height of the right triangle:"))
+base = int(input("Enter the base of the right triangle:"))
+print("The area of th right triangle with height:{0} and base {1} is {2}".format(height, base, 0.5 * height * base))
+print("The perimeter of th right triangle with height:{0} and base {1} is {2}".format(height, base,
+                                                                                      height + base + math.pow(
+                                                                                          height * height + base * base,
+                                                                                          0.5)))
